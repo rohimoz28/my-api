2fadb90199d48f739fe624ea061e3477194f8647
rohimoz28 <rohimoz28@gmail.com>
2021-11-04 19:12:24 +0700

    add info source code api

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php require 'layout/header.php' ?>
 
-<h1>Halaman Beranda</h1>
-<p>Ini adalah halaman index.php</p>
-<ul>
-  <li>1. Jadwal sholat</li>
-</ul>
+<div class="row">
+  <div class="col-md-10">
+    <h2 class="my-3">Keterangan / Source Code</h2>
+    <ul>
+      <li>API BMKG</li>
+      <p>Source Code : <a href="https://github.com/renomureza/cuaca-gempa-rest-api">cuaca-gempa-rest-api</a></p>
+      <li>API Klasemen Sepak bola</li>
+      <p>Source Code : <a href="https://github.com/azharimm/football-standings-api">football-standings-api</a></p>
+      <li>API Jadwal Sholat</li>
+      <p>Source Code <a href="https://github.com/lakuapik/jadwalsholatorg">jadwalsholatorg</a></p>
+    </ul>
+  </div>
+</div>
+
+
+
 <?php require 'layout/footer.php' ?>
